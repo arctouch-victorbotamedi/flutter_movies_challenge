c7159c05ef3254f32ad7a7f49fcfd861e02cb51d
Victor Botamedi <victor.botamedi@arctouch.com>
2019-03-27 10:45:35 -0300

    Implemented movie list component

---
+++ lib/view/components/movie_list.dart
@@ -0,0 +1,51 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:movies_challenge/module/movies_slice.dart';
+import 'package:movies_challenge/view/components/movie_list_item.dart';
+import 'package:movies_challenge/view/providers/movies_provider.dart';
+
+
+class MovieList extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() {
+    return _MoviesListHomePageState();
+  }
+}
+
+class _MoviesListHomePageState extends State<MovieList> {
+
+  @override
+  Widget build(BuildContext context) {
+    return _buildMovies(context);
+  }
+
+  Widget _buildMovies(BuildContext context) {
+    final moviesBloc = MoviesProvider.of(context);
+
+    return StreamBuilder<MoviesSlice>(
+      stream: moviesBloc.slice,
+      initialData: MoviesSlice.empty(),
+      builder: (context, snapshot) =>
+          ListView.builder(
+              padding: const EdgeInsets.all(16.0),
+              itemBuilder: (context, i) {
+                // Add a one-pixel-high divider widget before each row in theListView.
+                if (i.isOdd)
+                  return Divider();
+
+                // The syntax "i ~/ 2" divides i by 2 and returns an integer result.
+                // For example: 1, 2, 3, 4, 5 becomes 0, 1, 1, 2, 2.
+                // This calculates the actual number of word pairings in the ListView,
+                // minus the divider widgets.
+                final index = i ~/ 2;
+                moviesBloc.index.add(index);
+
+                var movie = snapshot.data.elementAt(index);
+                if (null == movie)
+                  return Center(child: CircularProgressIndicator());
+
+                return MovieListItem(movie);
+              }),
+    );
+  }
+}
--- lib/view/home_page.dart
+++ lib/view/home_page.dart
@@ -1,7 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:movies_challenge/module/movies_slice.dart';
-import 'package:movies_challenge/view/components/movie_list_item.dart';
-import 'package:movies_challenge/view/providers/movies_provider.dart';
+import 'package:movies_challenge/view/components/movie_list.dart';
 
 
 class HomePage extends StatelessWidget {
@@ -15,37 +13,7 @@ class HomePage extends StatelessWidget {
       appBar: new AppBar(
         title: new Text('Movies'),
       ),
-      body: _buildMovies(context),
-    );
-  }
-
-  Widget _buildMovies(BuildContext context) {
-    final moviesBloc = MoviesProvider.of(context);
-
-    return StreamBuilder<MoviesSlice>(
-      stream: moviesBloc.slice,
-      initialData: MoviesSlice.empty(),
-      builder: (context, snapshot) =>
-          ListView.builder(
-              padding: const EdgeInsets.all(16.0),
-              itemBuilder: (context, i) {
-                // Add a one-pixel-high divider widget before each row in theListView.
-                if (i.isOdd)
-                  return Divider();
-
-                // The syntax "i ~/ 2" divides i by 2 and returns an integer result.
-                // For example: 1, 2, 3, 4, 5 becomes 0, 1, 1, 2, 2.
-                // This calculates the actual number of word pairings in the ListView,
-                // minus the divider widgets.
-                final index = i ~/ 2;
-                moviesBloc.index.add(index);
-
-                var movie = snapshot.data.elementAt(index);
-                if (null == movie)
-                  return Center(child: CircularProgressIndicator());
-
-                return MovieListItem(movie);
-              }),
+      body: MovieList(),
     );
   }
 }
