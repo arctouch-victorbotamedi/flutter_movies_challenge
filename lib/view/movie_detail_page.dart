0caa3df57e98a6ce5f4c6b524408265ddc23c7c8
Victor Botamedi <victor.botamedi@arctouch.com>
2019-04-04 18:31:49 -0300

    Changed parallax to use a scroll controller

--- lib/view/movie_detail_page.dart
+++ lib/view/movie_detail_page.dart
@@ -8,6 +8,15 @@ import 'package:movies_challenge/view/components/cast_list.dart';
 import 'package:movies_challenge/view/components/movie_detail_header.dart';
 import 'package:gradient_app_bar/gradient_app_bar.dart';
 
+//class DeltaScrollController extends ScrollController {
+//
+//  double delta() {
+//    if (positions.length <= 2)
+//      return 0;
+//    var lastPosition = positions.elementAt(positions.length - 2).pixels;
+//    return position.pixels - lastPosition;
+//  }
+//}
 
 class MovieDetailPage extends StatefulWidget {
   final MovieRepository movieRepository;
@@ -24,16 +33,27 @@ class _MovieDetailsPage extends State<MovieDetailPage> {
   MovieRepository get _movieRepository => widget.movieRepository;
   Movie get _movie => widget.movie;
 
+  TrackingScrollController _scrollController = TrackingScrollController();
+
   @override
   void initState() {
     _movieDetailsBloc = MovieDetailsBloc(_movie, _movieRepository);
     _movieDetailsBloc.dispatch(Fetch());
+    _scrollController.addListener(() {
+      setState(() {
+        var offset = _scrollController.offset - _scrollController.initialScrollOffset;
+        var factor = offset / 2;
+        top -= factor;
+        scaledImageHeight -= factor;
+      });
+    });
     super.initState();
   }
 
   @override
   void dispose() {
     _movieDetailsBloc.dispose();
+    _scrollController.dispose();
     super.dispose();
   }
 
@@ -50,34 +70,25 @@ class _MovieDetailsPage extends State<MovieDetailPage> {
           backgroundColorEnd: theme.accentColor,
           title: new Text(_movie.title),
         ),
-        body: NotificationListener<ScrollUpdateNotification>(
-          onNotification: (notification) {
-            setState(() {
-              var factor = notification.scrollDelta / 2;
-              top -= factor;
-              scaledImageHeight -= factor;
-              print('Scale: $scaledImageHeight');
-              print('Top: $top');
-            });
-          },
-          child: Stack(
-            children: [
-              Positioned(
-                top : top <= 0 ? top : 0,
-                child: ArcImage(_movie.backdropUrl, imageHeight >= scaledImageHeight ? imageHeight : scaledImageHeight)
-              ),
-              ListView(
-                children: [
-                  Padding(
-                    padding: const EdgeInsets.only(left: 16, right: 16, top: 200.0),
-                    child: MovieDetailHeader(_movie),
-                  ),
-                  _overviewSection(theme),
-                  CastList(_movieDetailsBloc)
-                ],
-              )
-            ],
-          )
+        body: Stack(
+          children: [
+            Positioned(
+              top : top <= 0 ? top : 0,
+              child: ArcImage(_movie.backdropUrl, imageHeight >= scaledImageHeight ? imageHeight : scaledImageHeight)
+            ),
+            ListView(
+              physics: AlwaysScrollableScrollPhysics(parent:BouncingScrollPhysics()),
+              controller: _scrollController,
+              children: [
+                Padding(
+                  padding: const EdgeInsets.only(left: 16, right: 16, top: 200.0),
+                  child: MovieDetailHeader(_movie),
+                ),
+                _overviewSection(theme),
+                CastList(_movieDetailsBloc)
+              ],
+            )
+          ],
         )
     );
   }
